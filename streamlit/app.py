89c7d0304e41aca5bc2c062aa0f9834c2c05505b
MD FIRDOSH ALAM <mdfirdoshalam851202@gmail.com>
2025-11-02 14:23:06 +0530

    add streamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,21 +1,31 @@
 import streamlit as st
 import requests
 
-st.title("🎓 Student Performance Prediction")
+st.set_page_config(page_title="Student Form", page_icon="🎓")
+st.title("🎓 Student Data Submission (FastAPI + Streamlit)")
 
-# --- Collect inputs from user ---
-age = st.number_input("Age", min_value=1, max_value=120)
-gender = st.selectbox("Gender", ["Male", "Female", "Other"])
-p_class = st.text_input("Class (e.g., 10th)")
-study_hours = st.number_input("Study Hours per Day", min_value=0.0, max_value=24.0)
-attendance = st.slider("Attendance (%)", 0, 100, 90)
-math_score = st.slider("Math Score (%)", 0, 100, 85)
-science_score = st.slider("Science Score (%)", 0, 100, 80)
-english_score = st.slider("English Score (%)", 0, 100, 75)
-passed = st.checkbox("Passed")
+# API endpoint
+API_URL = "https://student-api-t2xw.onrender.com/create"
 
-if st.button("Predict"):
+with st.form("student_form"):
+    student_id = st.number_input("Roll No", min_value=1, step=1)
+    name = st.text_input("Name")
+    age = st.number_input("Age", min_value=5, max_value=25)
+    gender = st.selectbox("Gender", ["Male", "Female", "Other"])
+    p_class = st.text_input("Class", "10th")
+    study_hours = st.number_input("Study Hours", min_value=0.0, step=0.5)
+    attendance = st.slider("Attendance (%)", 0, 100, 75)
+    math_score = st.slider("Math (%)", 0, 100, 80)
+    science_score = st.slider("Science (%)", 0, 100, 82)
+    english_score = st.slider("English (%)", 0, 100, 78)
+    passed = st.checkbox("Passed?", value=True)
+
+    submit = st.form_submit_button("Submit")
+
+if submit:
     data = {
+        "student_id": student_id,
+        "name": name,
         "age": age,
         "gender": gender,
         "p_class": p_class,
@@ -27,11 +37,16 @@ if st.button("Predict"):
         "passed": passed
     }
 
-    res = requests.post("http://127.0.0.1:8000/predict", json=data)
-    
-    if res.status_code == 200:
-        result = res.json()
-        st.success(f"🎯 Prediction: {result['prediction']}")
-        st.json(result["input_data"])
-    else:
-        st.error(f"❌ Error: {res.text}")
+    try:
+        response = requests.post(API_URL, json=data)
+        if response.status_code == 200:
+            st.success("✅ Data submitted successfully!")
+            st.json(response.json())
+        else:
+            st.error(f"response_code:({response.status_code})")
+            if response.status_code ==400:
+                st.error("file already exists ")
+            elif response.status_code==201:
+                st.success("file successful create")
+    except Exception as e:
+        st.error(f"🚨 Connection Error: {e}")
